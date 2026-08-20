a058627469571ad8e14734b30d960c630ce54427
codemag33 <codemag33@users.noreply.github.com>
2026-08-20 07:49:22 +0000

    chore(android): версия 1.1 (versionCode 2)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.driver.app"
         minSdk = 26          // PiP RemoteAction доступен с API 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     signingConfigs {
